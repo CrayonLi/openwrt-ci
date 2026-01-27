a5d493e57bcab3e273b3000fc1f649e168a1ba8c
CrayonLi <84754186+CrayonLi@users.noreply.github.com>
2026-01-28 07:24:45 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -16,15 +16,3 @@ mkdir -p files/usr/bin/AdGuardHome
 AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_arm64 | awk -F '"' '{print $4}')
 wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
 chmod +x files/usr/bin/AdGuardHome/AdGuardHome
-
-# 提取并打包 kmod 软件源
-if [ -f "$BASE_PATH/extract_kmod_repo.sh" ]; then
-  chmod +x "$BASE_PATH/extract_kmod_repo.sh"
-  "$BASE_PATH/extract_kmod_repo.sh" "$BASE_PATH/$BUILD_DIR"
-fi
-# 复制 kmod 压缩包到 firmware 目录，便于发布
-find "$BASE_PATH/$BUILD_DIR/bin/targets/qualcommax/ipq60xx" -name "kmod-repo-*.tar.gz" -exec cp {} "$FIRMWARE_DIR/" \;
-
-if [[ -d $BASE_PATH/action_build ]]; then
-    make clean
-fi
